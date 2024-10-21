fbde8f64e331b24890bb8ef7f11d5857b48ec2d2
Luke Burton <lukesburton19@gmail.com>
2024-10-20 20:37:34 -0600

    add tmux-like multiplexer controls

--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,41 @@
 local wezterm = require 'wezterm'
 local config = {}
+
 config.font = wezterm.font 'JetBrains Mono'
 config.color_scheme = "Catppuccin Mocha"
+
+config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.keys = {
+    {
+        key = "'",
+        mods = 'LEADER',
+        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
+    },
+    {
+        key = "%",
+        mods = 'LEADER|SHIFT',
+        action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
+    },
+    {
+        key = "d",
+        mods = 'LEADER',
+        action = wezterm.action.CloseCurrentPane { confirm = true }
+    },
+    {
+        key = "c",
+        mods = 'LEADER',
+        action = wezterm.action.SpawnTab 'CurrentPaneDomain'
+    },
+}
+
+-- <leader> 1 through 8 switch to respective tab
+for i = 1, 9 do
+    table.insert(config.keys, {
+        key = tostring(i),
+        mods = 'LEADER',
+        action = wezterm.action.ActivateTab(i - 1),
+    })
+end
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
     config.default_prog = { 'pwsh.exe'}
