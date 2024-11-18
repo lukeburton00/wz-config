1581fb1ef2462595ccccc5ac8153ec90db121fe9
Luke Burton <=>
2024-11-18 10:43:43 -0700

    remove colorscheme

--- wezterm.lua
+++ wezterm.lua
@@ -7,7 +7,6 @@ if wezterm.config_builder then
 end
 
 config.font = wezterm.font 'JetBrains Mono'
-config.color_scheme = "Catppuccin Mocha"
 
 config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
