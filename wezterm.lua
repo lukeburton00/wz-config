2d470ba87179e5cece63afea90a9025949815db8
Luke Burton <lukesburton19@gmail.com>
2025-01-31 00:19:26 -0700

    stop ligatures

--- wezterm.lua
+++ wezterm.lua
@@ -2,6 +2,8 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.font = wezterm.font('JetBrains Mono')
+config.harfbuzz_features = {"calt=0", "clig=0", "liga=0"}
+
 config.color_scheme = 'Molokai'
 
 config.tab_bar_at_bottom = true
@@ -57,6 +59,10 @@ end
 
 if wezterm.target_triple:find("darwin") ~= nil then
     config.font_size = 14
+end
+
+if wezterm.target_triple:find("windows") ~= nil then
+    config.default_prog = { 'pwsh.exe'}
 end
 
 return config
