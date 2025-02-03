476209382f019a96cbf622f49d9c099bd025d3ff
Luke Burton <lukesburton19@gmail.com>
2025-02-02 20:20:52 -0700

    rearrange remaps into new file

---
+++ remaps.lua
@@ -0,0 +1,52 @@
+local wezterm = require 'wezterm'
+
+local remaps = {}
+
+remaps.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 2000 }
+remaps.keys = {}
+
+remaps.keys = {
+    {
+        key = "'",
+        mods = 'LEADER',
+        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
+    },
+    {
+        key = "%",
+        mods = 'LEADER|SHIFT',
+        action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
+    },
+    {
+        key = "d",
+        mods = 'LEADER',
+        action = wezterm.action.CloseCurrentPane { confirm = true }
+    },
+    {
+        key = "c",
+        mods = 'LEADER',
+        action = wezterm.action.SpawnTab 'CurrentPaneDomain'
+    },
+    {
+        key = 'h',
+        mods = 'LEADER',
+        action = wezterm.action.SwitchToWorkspace {
+            name = 'default',
+        }
+    },
+    {
+        key = "Tab",
+        mods = "CTRL",
+        action = wezterm.action.DisableDefaultAssignment,
+    },
+}
+
+
+for i = 1, 9 do
+    table.insert(remaps.keys, {
+        key = tostring(i),
+        mods = 'LEADER',
+        action = wezterm.action.ActivateTab(i - 1),
+    })
+end
+
+return remaps
--- wezterm.lua
+++ wezterm.lua
@@ -1,60 +1,19 @@
 local wezterm = require 'wezterm'
-local config = {}
+local remaps = require 'remaps'
 
+local config = {}
 config.font = wezterm.font('JetBrains Mono')
 config.harfbuzz_features = {"calt=0", "clig=0", "liga=0"}
+
 config.color_scheme = 'Molokai'
+
+config.leader = remaps.leader
+config.keys = remaps.keys
 
 config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
 
 config.max_fps = 240
-
-config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 2000 }
-config.keys = {}
-
-config.keys = {
-    {
-        key = "'",
-        mods = 'LEADER',
-        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
-    },
-    {
-        key = "%",
-        mods = 'LEADER|SHIFT',
-        action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
-    },
-    {
-        key = "d",
-        mods = 'LEADER',
-        action = wezterm.action.CloseCurrentPane { confirm = true }
-    },
-    {
-        key = "c",
-        mods = 'LEADER',
-        action = wezterm.action.SpawnTab 'CurrentPaneDomain'
-    },
-    {
-        key = 'h',
-        mods = 'LEADER',
-        action = wezterm.action.SwitchToWorkspace {
-            name = 'default',
-        }
-    },
-    {
-        key = "Tab",
-        mods = "CTRL",
-        action = wezterm.action.DisableDefaultAssignment,
-    },
-}
-
-for i = 1, 9 do
-    table.insert(config.keys, {
-        key = tostring(i),
-        mods = 'LEADER',
-        action = wezterm.action.ActivateTab(i - 1),
-    })
-end
 
 if wezterm.target_triple:find("darwin") ~= nil then
     config.font_size = 14
